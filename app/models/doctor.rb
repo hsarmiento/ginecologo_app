f9bcdf5871e42c186da73022f687826163032203
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2013-01-17 17:50:43 -0300

    Fixed doctor model and added link to root path in nav bar

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,6 @@
 class Doctor < ActiveRecord::Base
   attr_accessible :clinic_id, :last_name, :name, :phone, :speciality
   belongs_to :clinic
-  has_many :schedules, :reserves
+  has_many :schedules
+  has_many :reserves
 end
