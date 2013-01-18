8774fdc5ee9a1d721abc27a6411e4470263b13d3
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2013-01-18 12:35:49 -0300

    Added clinics in index page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
 	def index
-		
+		@clinics = Clinic.all
 	end
 end
--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -1,4 +1,5 @@
 class Reserve < ActiveRecord::Base
   attr_accessible :available, :datetime, :doctor_id, :patient_id
-  belongs_to :doctor, :patient
+  belongs_to :doctor
+  belongs_to :patient
 end
